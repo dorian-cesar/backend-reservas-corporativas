a3b13b4549e983dbef45d07bf0ed4e1dc6dc3a46
jesuSando <correoepicgames9@gmail.com>
2025-12-16 13:30:16 -0300

    feat: se agrega middleware de autenticacion a importacion de archivos csv

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -2,18 +2,23 @@ import { Router } from "express";
 import { CSVController } from "../controllers/upload.controller";
 import { uploadUsersCSV } from "../middleware/upload-user.middleware";
 import { uploadPassengersCSV } from "../middleware/upload-passenger.middleware";
+import { authenticateJWT, authorizeRoles } from "../middleware/auth.middleware";
 
 
 const router = Router();
 
 router.post(
     "/users/csv",
+    authenticateJWT,
+    authorizeRoles("superuser"),
     uploadUsersCSV.single("file"),
     CSVController.uploadUser
 );
 
 router.post(
     "/passengers/csv",
+    authenticateJWT,
+    authorizeRoles("superuser"),
     uploadPassengersCSV.single("file"),
     CSVController.uploadPassenger
 );
