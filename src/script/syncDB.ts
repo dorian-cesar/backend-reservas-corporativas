7b375536b1a9319d5be7de823efcf5fd8b58cd31
Diegowigo <dwigodski@gmail.com>
2026-07-23 11:13:08 -0400

    chore: eliminar script syncDB y limpiar scripts en package.json

--- src/script/syncDB.ts
+++
@@ -1,8 +0,0 @@
-import { connectDB, sequelize } from '../database';
-
-(async () => {
-    await connectDB();
-    await sequelize.sync({ alter: true });
-    console.log('¡Base de datos sincronizada y actualizada con los modelos nuevos sin eliminar datos!');
-    process.exit(0);
-})();
